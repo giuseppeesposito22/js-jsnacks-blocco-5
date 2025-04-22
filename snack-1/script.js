8f10b0c3282dd2ae7e2f810438530be84e6a7c28
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-22 15:06:52 +0200

    snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,8 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (const name of names) {
+  console.log(name);
+}
