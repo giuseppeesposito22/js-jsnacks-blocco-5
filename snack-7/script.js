121e8da06c676688bb26a91b7a2c0225c2c01e6c
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-22 15:42:03 +0200

    snack 7 short

--- snack-7/script.js
+++ snack-7/script.js
@@ -22,9 +22,6 @@ const students = [
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
 
-const studentById = (id) => {
-  const student = students.filter((student) => student.id === id);
-  return student;
-};
+const studentById = (id) => students.filter((student) => student.id === id)[0];
 
 console.log(studentById(2));
